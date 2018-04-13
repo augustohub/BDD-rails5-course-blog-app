199719dffdc7d156ef4bcce41b6855442e2415e1
Augusto Hubert <augustohub@gmail.com>
2018-04-12 23:56:00 -0300

    fix syntax on listing_articles_spec

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature 'Listing Articles' do
 
   before do
-    @article1 = Article.new(title: 'Article 1', body: 'Body of Article 1')
-    @article2 = Article.new(title: 'Article 2', body: 'Body of Article 2')
+    @article1 = Article.create(title: 'Article 1', body: 'Body of Article 1')
+    @article2 = Article.create(title: 'Article 2', body: 'Body of Article 2')
   end
 
   scenario 'A user list all articles' do
