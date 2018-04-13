5c39a2e6719a301f3cdcd6b21af3b52da99ad048
Augusto Hubert <augustohub@gmail.com>
2018-04-13 00:09:29 -0300

    test for no articles on index

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -19,4 +19,23 @@ RSpec.feature 'Listing Articles' do
     expect(page).to have_content(@article2.body)
 
   end
+
+  scenario 'A user has no articles' do
+    Article.delete_all
+
+    visit '/'
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_link(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+
+    within("#no-articles") do
+      expect(page).to have_content('No articles created')
+    end
+
+  end
 end
