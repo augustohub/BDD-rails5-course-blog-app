7699ea6f15e4a368e44c2f32f796124e0f9b0b7d
Augusto Hubert <augustohub@gmail.com>
2018-04-13 21:23:43 -0300

    creates fail update article scenario test

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -15,10 +15,23 @@ RSpec.feature 'Editing an Article' do
     fill_in 'Title', with: 'Updated Article Title'
     fill_in 'Body', with: 'Updated Article content Body'
     click_button 'Update'
-    
+
     expect(page).to have_content('Article has been updated')
     expect(page).to have_content(h2, 'Updated Article Title')
     expect(page).to have_content('Updated Article content Body')
     expect(page.current_path).to eql(article_path(@article)
+  end
+
+  scenario 'user fail to update an article' do
+    visit '/'
+    click_link @article.title
+
+    click_link 'Edit'
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: ''
+    click_button 'Update'
+    
+    expect(page).to have_content('Article has not been updated')
+    expect(page.current_path).to eql(edit_article_path(@article)
   end
 end
